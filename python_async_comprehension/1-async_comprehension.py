8d4fc10c5e3143956e8b2904575c58fb0a30b657
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-01 21:43:48 +0000

    Task error fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,10 +4,10 @@ async_Comprehensions module
 """
 import asyncio
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
+
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    """
+    """async_Comprehensions module"""
     return [number async for number in async_generator()]
