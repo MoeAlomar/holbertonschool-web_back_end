ffe21b782328a1ee0b902c76f73348d754a8a207
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-11 07:30:27 +0000

    Task 0 done

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" In this module i will implement a method that will help in pagination
+specifically in calculating the starting and ending index to reach
+the starting point of a page that you choose.
+"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """in this method we will calculate the start index and end index
+    to reach a specific page with a limited page size.
+
+    Args:
+        page (int): the page number we want to reach (starting from 1).
+        page_size (int): the size of each page.
+
+    Returns: a tuple combination of the start_index and the end_index.
+
+    Formula:
+        n: (number of page)
+        p: (page size)
+        start_index = (n-1) * p
+        end_index = n * p
+    """
+    
+    start_index = (page-1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
---
+++ pagination/README.md
@@ -0,0 +1,1 @@
+# Pagination | Web back end repo
