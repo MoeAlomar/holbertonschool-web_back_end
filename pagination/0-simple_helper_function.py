8475ed39dc308ac8809f855692caab2010b14df2
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-11 07:31:18 +0000

    forgot the newline :)

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -20,7 +20,7 @@ def index_range(page: int, page_size: int) -> tuple:
         start_index = (n-1) * p
         end_index = n * p
     """
-    
+
     start_index = (page-1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
