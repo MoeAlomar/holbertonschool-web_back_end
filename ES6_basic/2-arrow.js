c97de2d911190888c0b44d73aabab1efa210883a
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-20 17:35:17 +0000

    Task 2 fix trial

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,10 @@
-export default function getNeighborhoodsList() {
+export default class NeighborhoodsList {
+  constructor() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
   }
+
+  addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
