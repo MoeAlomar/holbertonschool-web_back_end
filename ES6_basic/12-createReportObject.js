23e61f515fa8195be8b62ebc1622ac44faf8bffc
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-23 16:44:35 +0000

    Task 12 fix try...

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
     return {
       allEmployees: {
-        employeesList,
+        ...employeesList,
       },
       getNumberOfDepartments(employees) {
         return Object.keys(employees).length;
