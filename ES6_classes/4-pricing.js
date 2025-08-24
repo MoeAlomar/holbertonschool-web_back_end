1b3f4b558840028cdc149c932feef840eceeeb5f
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-24 08:36:27 +0000

    Task 4 init commit

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, Currency) {
+    this._amount = amount;
+    this._currency = Currency;
+  }
+
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
