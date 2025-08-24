f4c2f3b424768cae9a1e5b25b962bf47dbf6888a
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-24 08:53:47 +0000

    Task 5 fix try ?

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,21 +1,23 @@
 export default class Building {
     constructor(sqft) {
-
+      // Store in underscore attribute version
       this._sqft = sqft;
       
-      if (this.constructor === Building) {
-        throw new Error('Cannot instantiate abstract class Building');
-      }
-      
-      if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+      // Only check for abstract method implementation if this is a subclass
+      if (this.constructor !== Building) {
+        // Check if extending class implements required abstract method
+        if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+          throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
       }
     }
   
+    // ES6 Getter for sqft attribute
     get sqft() {
       return this._sqft;
     }
   
+    // Abstract method that must be overridden by subclasses
     evacuationWarningMessage() {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
