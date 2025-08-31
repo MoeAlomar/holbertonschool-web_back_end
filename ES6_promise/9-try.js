910e1bdfdd25cc08e57700e3f6c9e88b4484384d
MoeAlomar <Mohammedahalomar@gmail.com>
2025-08-31 10:31:51 +0000

    Task 9 simple try catch and finally block

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } 
+    catch (err) {
+      queue.push(err.toString());
+    } 
+    finally {
+      queue.push('Guardrail was processed');}
+  
+    return queue;
+  }
+  
