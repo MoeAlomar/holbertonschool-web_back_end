cb834e4f6a0834714a802ea9b5b2c7cc29b673b1
MoeAlomar <Mohammedahalomar@gmail.com>
2025-09-04 16:49:06 +0000

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.setEncoding("utf8");
+
+process.stdin.on("data", (data) => {
+  const input = data.trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+});
+
+process.on("SIGINT", () => {
+  process.stdout.write("This important software is now closing\n");
+  process.exit();
+});
