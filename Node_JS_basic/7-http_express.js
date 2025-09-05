dd844a4a907e0b55080c095b13bc7e55d18a509b
MoeAlomar <Mohammedahalomar@gmail.com>
2025-09-05 17:18:48 +0000

    Task 7 fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,38 @@
 const express = require('express');
+
 const countStudents = require('./3-read_file_async');
 
+const databaseFile = process.argv[2];
 const app = express();
-const DB_PATH = process.argv[2];
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
-
 app.get('/students', (req, res) => {
-    res.set('Content-Type', 'text/plain');
-    countStudents(DB_PATH)
-        .then((data) => {
-            res.send(`This is the list of our students\n${data}`);
-        })
-        .catch((err) => {
-            res.end(`This is the list of our students\n${err.message}`);
-        });
+  res.set('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+
+  if (!databaseFile) {
+    res.end('Cannot load the database');
+    return;
+  }
+
+  const originalLog = console.log;
+  const lines = [];
+  console.log = (...args) => { lines.push(args.join(' ')); };
+
+  countStudents(databaseFile)
+    .then(() => {
+      console.log = originalLog;
+      res.end(lines.join('\n'));
+    })
+    .catch((err) => {
+      console.log = originalLog;
+      res.end(err.message);
+    });
 });
 
 app.listen(1245);
-
 module.exports = app;
